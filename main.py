e5b287f8e1a6974e280e45baa39fbf8de8846f4b
Your Name <medhaav.mahesh@gmail.com>
2025-03-03 12:18:49 -0800

    Add ability to search and upload books directly from project gutenberg

--- main.py
+++ main.py
@@ -9,8 +9,22 @@ from bs4 import BeautifulSoup
 
 app = FastAPI()
 
+@app.post("/upload_pg_book/")
+async def upload_book_from_pg(search_term: str, db: Session = Depends(get_db)):
+    book_path = searcher.search_book(search_term)
+    if book_path:
+        contents = searcher.extract_book_from_epub(book_path)
+        new_book = Book(title=contents['title'], author=contents['author'], text=contents['text'])
+        db.add(new_book)
+        db.commit()
+        db.refresh(new_book)
+        os.remove(book_path)
+        return {f"message: {contents['title']} by {contents['author']} has been uploaded"}
+    else:
+        raise HTTPException(status_code=404, detail="Book can't be found")
+
 @app.post("/upload_book/")
-async def upload_book(file: UploadFile, db: Session = Depends(get_db)):
+async def upload_book(file: UploadFile=File(...), db: Session = Depends(get_db)):
     if file.content_type == 'application/epub+zip':
         contents = searcher.extract_book_from_epub(f"../../Downloads/{file.filename}")
         new_book = Book(title=contents['title'], author=contents['author'], text=contents['text'])
