0289ca50aa1c3a3097e6d8e34824c8e3024e7d87
Your Name <medhaav.mahesh@gmail.com>
2025-03-03 21:27:13 -0800

    More accurate character extracter - Need to speed up

--- analyzer.py
+++ analyzer.py
@@ -3,9 +3,12 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
+spacy.require_gpu()
+nlp = spacy.load("en_core_web_trf")
 
 nltk.download("punkt")
+
+exclude_characters = {"gutenberg"}
 
 def clean_text(text):
     words = word_tokenize(text)
@@ -14,4 +17,4 @@ def clean_text(text):
 
 def extract_characters(text):
     doc = nlp(text)
-    return [ent.text for ent in doc.ents if ent.label_ == "PERSON"]
+    return [ent.text for ent in doc.ents if ent.label_ == "PERSON" and ent.text.lower() not in exclude_characters]
